55d1bb1307b74a708271d0fe6ce4430da5459421
AmanYize <amanexo19@gmail.com>
2023-12-28 06:55:13 -0300

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 function App() {
+  
   const isEmployee = true;
+  const [role, setRole] = useState('dev');
   return (
     <div className="App">
      {isEmployee ? 
      <div>
-      <Employee />
+       <input type="text" onChange={(e)=>{
+              setRole(e.target.value);
+                console.log(e.target.value)
+              }}/>
+        <Employee name="Aman" role="Intern"/>
+        <Employee name="Luna" role={role}/>
+        <Employee name="Astral" />
       </div> : 
       <h2>Not Employee</h2>
       }
