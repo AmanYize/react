5a7918ff61228e495c366e7c3ca8179f072efd4d
AmanYize <amanexo19@gmail.com>
2023-12-28 11:10:16 -0300

    working with some tailwind

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ function App() {
   const isEmployee = true;
   const [role, setRole] = useState('dev');
   return (
-    <div className="App bg-red-300">
+    <div className="App">
      {isEmployee ? 
      <div>
        <input type="text" onChange={(e)=>{
               setRole(e.target.value);
                 console.log(e.target.value)
               }}/>
-        <Employee name="Aman" role="Intern" />
-        <Employee name="Luna" role={role}/>
-        <Employee name="Astral" />
+        <div className="flex flex-wrap justify-center">
+        <Employee name="Draco" role="Intern" img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <Employee name="Luna" role={role} img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <Employee name="Astral" img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <Employee name="Draco" role="Intern" img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <Employee name="Luna" role={role} img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <Employee name="Astral" img="https://images.pexels.com/photos/980859/pexels-photo-980859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        </div>
       </div> : 
       <h2>Not Employee</h2>
       }
